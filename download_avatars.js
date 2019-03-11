a9098ded10f970d9388188351a265a26b6aa96b9
Melissa L <get.psychick@gmail.com>
2019-03-11 23:55:56 +0000

    Added parsing for user avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,33 @@
 var request = require('request');
+var secretReq = require('./secret');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${secretReq.GITHUB_TOKEN}`
+    }
+  };
+
+  
+  
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  var users = JSON.parse(result);
+  for (var user of users) {
+    console.log(user.avatar_url);
+  }
+
+  
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+});
+
